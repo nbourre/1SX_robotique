f75b3c1213c9c6c5afaec4818f2b0b0fc744c5f7
Nick B <nbourrereg@hotmail.com>
2022-10-04 17:21:52 -0400

    Modification du projet de son pour visualiser les données plus facilement

--- cours_06_capteurs/ranger_sound/ranger_sound.ino
+++ cours_06_capteurs/ranger_sound/ranger_sound.ino
@@ -1,13 +1,17 @@
+/*
+* Requirements : an analog device connected on A1
+* Open Serial Plotter in tools to view the result
+*/
+
 int soundValue = 0;
 int sound_avg = 0;
 long sound_sum = 0;
 int sound_count = 0;
-int sample_size = 64;
+int sample_size = 32;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
-
 }
 
 void loop() {
@@ -15,12 +19,13 @@ void loop() {
   soundValue = analogRead(A1);
   sound_sum += soundValue;
   if (sound_count == sample_size - 1){
-    sound_avg = sound_sum >> 6; // right shift de 2^5 (comme divisé par 32)
+    sound_avg = sound_sum >> 5; // right shift de 2^5 (comme divisé par 32)
     sound_count = 0;
     sound_sum = 0;
+    Serial.print("max:1024,min:0,");
     Serial.print("avg:");
     Serial.print(sound_avg);
-    Serial.println(";");
+    Serial.println(",");
   }
   
   sound_count = (sound_count + 1) % sample_size;
