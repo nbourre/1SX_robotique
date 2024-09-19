8c01b693b758ed2c1f79a62d2d720f1a60fe52f0
Nick B <nbourrereg@hotmail.com>
2024-09-19 10:11:46 -0400

    Ajout de fonctions de simulation

--- modele_base_etat_tache/modele_base_etat_tache.ino
+++ modele_base_etat_tache/modele_base_etat_tache.ino
@@ -66,3 +66,59 @@ void modeleTask(unsigned long ct)
     // Code à exécuter à chaque appel de la fonction
     // Mise à jour des variables
 }
+
+// Fonction qui simule la distance entre deux seuils
+// Elle interpolle la valeur de la distance à une fréquence
+// étable avec rate à un pas donnée par step
+
+// À utiliser comme si c'était un hc-sr04
+// donc hc.getDistance() devient simGetDistance()
+int simGetDistance() {
+    static int distance = 5;    // Valeur initiale
+    static int target = 25;     // Valeur cible
+    static int step = 1;        // Pas de l'interpolation
+    static unsigned long previousTime = 0;
+    static unsigned long rate = 250; // Taux de rafraîchissement
+
+    if (millis() - previousTime < rate)
+    {
+        return distance;
+    }
+
+    previousTime = millis();
+
+    if (distance < target)
+    {
+        step = 1;
+    }
+    else if (distance > target)
+    {
+        step = -1;
+    }
+
+    distance += step;
+
+
+    return distance;
+}
+
+// Fonction qui simule l'interrupteur
+// À toutes les X secondes, elle change l'état de l'interrupteur
+// À utiliser comme si c'était un bouton poussoir
+// donc digitalRead(pin) devient simDigitalRead(pin)
+int simDigitalRead(int pin) {
+    static int state = 0;
+    static unsigned long previousTime = 0;
+    static unsigned long rate = 5000; // Taux de rafraîchissement
+
+    if (millis() - previousTime < rate)
+    {
+        return state;
+    }
+
+    previousTime = millis();
+
+    state = !state;
+
+    return state;
+}
