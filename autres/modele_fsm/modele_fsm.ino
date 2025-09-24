8535afb7ea5bb974704414cf64aefb594ab874c1
Nick B <nbourrereg@hotmail.com>
2025-09-24 08:29:29 -0400

    MAJ avec commentaire

--- autres/modele_fsm/modele_fsm.ino
+++ autres/modele_fsm/modele_fsm.ino
@@ -1,5 +1,12 @@
+enum AppState {STOP, STATE_A};
 
+AppState appState = STOP;
+
+unsigned long currentTime = 0;
+
+// Les pragma region permettent de créer des zones de code fermable
 #pragma region Modèles
+
 void xState(unsigned long cT) {
   static unsigned long lastTime = 0;
   const int rate = 500;
@@ -21,7 +28,8 @@ void xState(unsigned long cT) {
 
   // Code de job à faire
 
-  // Remplacer par la formule pour effectuer la transition
+  // Il s'agit de la transition qui permet de sortir de l'état
+  // Qu'est-ce qui fait que l'on sort de l'état?
   bool transition = false;
 
   // Il est possible d'avoir plusieurs transitions
@@ -33,7 +41,54 @@ void xState(unsigned long cT) {
   }
 }
 
+// Modèle de tâche qui ne retourne aucune valeur.
+void xTask(unsigned long ct) {
+  static unsigned long lastTime = 0;
+  unsigned long rate = 500;
+  
+  if (ct - lastTime < rate) {
+    return;
+  }
+  
+  lastTime = ct;
+  
+  // Faire le code de la tâche ici
+  
+}
+
+// Modèle de tâche avec retour de valeur.
+// Exemple d'utilisation :
+//   - Mesure de distance, température, etc.
+int xTaskWithReturn(unsigned long ct) {
+  static unsigned long lastTime = 0;
+  unsigned long rate = 500;
+  static int lastResult = 0;
+  int result = 0;
+  
+  if (ct - lastTime < rate) {
+    return result;
+  }
+  
+  lastTime = ct;
+  
+  // Faire le code de la tâche ici
+  result = 1;
+  
+  lastResult = result;
+  
+  return result;  
+}
+
 #pragma endregion
+
+void stateManager(unsigned long ct) {
+  // Adapter selon votre situation!
+  switch (appState) {
+    case STOP:
+      
+      break;
+  }
+}
 
 #pragma region setup-loop
 void setup() {
@@ -43,7 +98,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentTime = millis();
+  
+  stateManager(currentTime);
 
 }
 #pragma endregion
